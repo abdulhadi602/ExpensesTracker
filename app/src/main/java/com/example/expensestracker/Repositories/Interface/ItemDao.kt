9ef83c20d6c760b75d3fc5c9c9790fc7a8731442
abdulhadi602 <ahadi3608@gmail.com>
2021-09-01 13:19:54 +0000

    Adding date to list

--- app/src/main/java/com/example/expensestracker/Repositories/Interface/ItemDao.kt
+++ app/src/main/java/com/example/expensestracker/Repositories/Interface/ItemDao.kt
@@ -10,13 +10,13 @@ interface ItemDao{
     @Insert
     suspend fun InsertItem(item : Item)
 
-    @Query("SELECT * FROM items_table ORDER BY id ASC")
+    @Query("SELECT * FROM items_table ORDER BY date DESC")
     suspend fun getItems(): List<Item>
 
     @Query("DELETE FROM items_table")
     suspend fun deleteItems()
 
-    @Query("SELECT * from items_table WHERE date BETWEEN :startDate AND :finishDate")
+    @Query("SELECT * from items_table WHERE date BETWEEN :startDate AND :finishDate ORDER BY date DESC")
     suspend fun getTodaysItems(startDate: Long,finishDate : Long) : List<Item>
 
     @Update
--- app/src/main/java/com/example/expensestracker/RewardsAdapter.kt
+++ app/src/main/java/com/example/expensestracker/RewardsAdapter.kt
@@ -1,13 +1,10 @@
 package com.example.skyjobtest.Activities
 
-import android.content.AsyncQueryHandler
 import android.content.Context
 import android.os.Handler
-import android.os.Message
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.view.textclassifier.ConversationActions
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.example.expensestracker.R
@@ -15,17 +12,21 @@ import com.example.expensestracker.Repositories.Entitiy.Item
 import com.example.expensestracker.Utils.AlertState
 import com.example.expensestracker.Utils.ItemAlert
 import com.google.android.material.floatingactionbutton.FloatingActionButton
+import java.text.SimpleDateFormat
+import java.util.*
+import kotlin.collections.ArrayList
 
 
-class ItemsAdapter(con: Context,handler: Handler) : RecyclerView.Adapter<ItemsAdapter.ViewHolder>() {
+class ItemsAdapter(con: Context, handler: Handler) : RecyclerView.Adapter<ItemsAdapter.ViewHolder>() {
     var itemList: List<Item> = ArrayList()
     var con: Context
     val handler : Handler
+    var lastDateItem: Date? =null
     init {
         this.handler = handler
         this.con = con
     }
-    fun setrewardsList(list : List<Item>){
+    fun setrewardsList(list: List<Item>){
         this.itemList = list
         notifyDataSetChanged()
     }
@@ -43,15 +44,39 @@ class ItemsAdapter(con: Context,handler: Handler) : RecyclerView.Adapter<ItemsAd
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         val temp = itemList[position]
-            holder.itemName.text = temp.itemName
-            holder.itemPrice.text = temp.itemPrice.toString()
+        checkDate(position,holder,temp)
+        holder.itemName.text = temp.itemName
+        holder.itemPrice.text = temp.itemPrice.toString()
         holder.itemView.setOnClickListener {
-            ItemAlert(con , handler, AlertState.UPDATE,temp).show()
+            ItemAlert(con, handler, AlertState.UPDATE, temp).show()
         }
         holder.deleteBtn.setOnClickListener {
-            ItemAlert(con , handler, AlertState.DELETE,temp).show()
+            ItemAlert(con, handler, AlertState.DELETE, temp).show()
         }
+
     }
+    fun checkDate(pointer : Int,holder : ViewHolder,item : Item){
+//        if(pointer == 0){
+//            lastDateItem == null
+//        }
+//        if(lastDateItem!= null){
+//            if(lastDateItem!! isDateDifferent item.date){
+//                holder.itemDate.visibility = View.VISIBLE
+//                holder.itemDate.text = item.date.formatDate()
+//            }
+//        }
+        holder.itemDate.visibility = View.VISIBLE
+        holder.itemDate.text = item.date.formatDate()
+       // lastDateItem = item.date
+    }
+    infix fun Date.isDateDifferent(itemDate: Date) : Boolean{
+        return this.formatDate().equals(itemDate.formatDate())
+    }
+    fun Date.formatDate() : String{
+        val sdf = SimpleDateFormat("dd MMM yyy", Locale.getDefault())
+        return sdf.format(this)
+    }
+
 
 
     class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
@@ -59,6 +84,7 @@ class ItemsAdapter(con: Context,handler: Handler) : RecyclerView.Adapter<ItemsAd
         var itemName: TextView
         var itemPrice: TextView
         var deleteBtn : FloatingActionButton
+        var itemDate : TextView
 
 
         init {
@@ -66,6 +92,7 @@ class ItemsAdapter(con: Context,handler: Handler) : RecyclerView.Adapter<ItemsAd
             this.itemName = itemView.findViewById(R.id.itemName)
             this.itemPrice = itemView.findViewById(R.id.itemPrice)
             this.deleteBtn = itemView.findViewById(R.id.deleteBtn)
+            this.itemDate = itemView.findViewById(R.id.itemDate)
         }
 
 
